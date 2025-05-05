73175a7cd7d47cf983eb1947b08c63883196723e
ankitpotdar <ankitpotdar@microsoft.com>
2025-05-04 19:18:43 -0700

    boilerplate setup

---
+++ src/components/Counter.tsx
@@ -0,0 +1,37 @@
+import { AddFilled, SubtractFilled } from "@fluentui/react-icons";
+import { Button, tokens } from "@fluentui/react-components";
+
+import styled from "styled-components";
+import { useState } from "react";
+
+const Counter:React.FC = ()=>{
+
+      const [count, setCount] = useState(0);
+    return (    <Container>
+          <Button
+            shape="circular"
+            icon={<AddFilled />}
+            onClick={() => {
+              setCount((count) => count + 1);
+            }}
+          />
+          {count}
+          <Button
+            shape="circular"
+            icon={<SubtractFilled />}
+            onClick={() => {
+              setCount((count) => count - 1);
+            }}
+          />
+          
+        </Container>)
+}
+
+export default Counter
+
+
+const Container = styled.div`
+display:flex;
+align-items: center;
+gap: ${tokens.spacingHorizontalL}
+`;
